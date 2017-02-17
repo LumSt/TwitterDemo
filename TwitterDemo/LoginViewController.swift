6658d7229f64f7252c884dd6c87e6725d3684286
Lum Situ <lumsitu@Lums-MacBook-Pro.local>
2017-02-16 20:26:52 -0800

    Setup

---
+++ TwitterDemo/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  TwitterDemo
+//
+//  Created by Lum Situ on 2/16/17.
+//  Copyright © 2017 Lum Situ. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OnLoginButton(_ sender: Any) {
+        let twitterClient = BDBOAuth1SessionManager(baseURL: NSURL(string:"https://api.twitter.com") as URL!, consumerKey: "6AIJcovONDCExACdSFRyb41Qp", consumerSecret: "wwRyhcedhVsN0ARnUarQzgZF0b5BkHUPfB6e1dbWWw4cbrIgjL")
+        
+        twitterClient?.fetchRequestToken(withPath: "oauth/request_token", method: "GET", callbackURL: nil, scope: nil, success: { (requestToken: BDBOAuth1Credential?) in
+            print("I got a token!")
+            print(requestToken?.token! ?? "No token!")
+            
+            let url = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken?.token)")
+            print(url ?? "No url!")
+            UIApplication.shared.openURL((url as? URL)!)
+            
+        }, failure: { (error: Error?) in
+            print("error: \(error?.localizedDescription)")
+        })
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
